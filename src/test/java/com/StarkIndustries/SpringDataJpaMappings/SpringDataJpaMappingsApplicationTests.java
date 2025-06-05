4dc055c498e3dba62052270ff27565ac77665eac
Aditya S Kelaskar <kelaskaraditya1@gmail.com>
2025-06-05 11:30:52 +0530

    initial commit

---
+++ src/test/java/com/StarkIndustries/SpringDataJpaMappings/SpringDataJpaMappingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StarkIndustries.SpringDataJpaMappings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaMappingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
